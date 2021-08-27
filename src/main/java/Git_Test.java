78c45c465720c62e2120287e3c71542cde130071
JianZhenShuo <JianZS@2021AAA.com>
2021-08-27 23:46:11 +0800

    first commit

---
+++ src/main/java/Git_Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author Xanthuim
+ * @version 1.0
+ * @date 2019/3/21 15:11
+ */
+public class Git_Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
